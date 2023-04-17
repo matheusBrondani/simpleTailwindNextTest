a42a51cc72ccba935817cf873d706d8ca8d4f882
Matheus Brondani <matt.brondani@gmail.com>
2023-04-16 22:23:01 -0300

    Adding pageList component and update css files

---
+++ src/app/pageList.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { useState, useEffect } from "react";
+
+
+
+const MOCK_DATA = [
+  { id: 1, title: "Page 1" },
+  { id: 2, title: "Page 2" },
+  { id: 3, title: "Page 3" },
+];
+
+const PageList = () => {
+  const [pages, setPages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchPages();
+  }, []);
+
+  const fetchPages = async () => {
+    // mock api call to and to show spinner for two seconds
+    const data = await new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(MOCK_DATA);
+      }, 2000);
+    });
+
+    setPages(data);
+  
+    setLoading(false);
+  };
+
+  return (
+    <div>
+      {loading ? (
+        <div className="loading-indicator"></div>
+      ) : (
+        <ul>
+          {pages.map((page) => (
+            <li key={page.id}>{page.title}</li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default PageList;
